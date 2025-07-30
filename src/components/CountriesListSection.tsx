8082974a921421f174674c247b8d410182bffc13
MrSeager <seager1993@gmail.com>
2025-07-30 11:12:01 +0300

    7/30/2025

--- src/components/CountriesListSection.tsx
+++ src/components/CountriesListSection.tsx
@@ -43,16 +43,16 @@ const CountriesListSection: FC<CountriesListSectionProps> = ({ countriesList, ha
     return (
         <Col lg={9} xs={12} className='py-5 d-flex flex-column gap-4'>
             <Row className='cs-tc-one border-bottom pb-2'>
-                <Col lg={1} xs={2} className='p-0'>
+                <Col lg={2} xs={2} className='p-0'>
                     <h3 className='h6'>Flag</h3>
                 </Col>
-                <Col lg={3} xs={4} className='p-0'>
+                <Col md={4} xs={5} className='p-0'>
                     <h3 className='h6'>Name</h3>
                 </Col>
-                <Col lg={3} xs={4} className='p-0'>
+                <Col lg={2} md={4} xs={5} className='p-0'>
                     <h3 className='h6'>Population</h3>
                 </Col>
-                <Col lg={3} xs={2} className='p-0'>
+                <Col md={2} xs={0} className='p-0 d-none d-md-block'>
                     <h3 className='h6'>Area (km²)</h3>
                 </Col>
                 <Col lg={2} xs={0} className='p-0 d-none d-lg-block'>
